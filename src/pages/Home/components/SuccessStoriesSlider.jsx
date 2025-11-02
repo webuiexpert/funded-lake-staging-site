2d1540f22c967835b60d18cd1a8aeea7370bdfd0
shankarbhatia <shankarbhatia022@gmail.com>
2025-11-02 19:56:35 +0530

    fix auto hieght bug

--- src/pages/Home/components/SuccessStoriesSlider.jsx
+++ src/pages/Home/components/SuccessStoriesSlider.jsx
@@ -14,12 +14,13 @@ import { Pagination, Navigation } from "swiper/modules";
 
 function SuccessStoriesSlider() {
   return (
-    <div className=" w-full max-w-7xl mx-auto py-10 lg:mb-16 md:mb-10 mb-6">
+    <div className=" w-full max-w-7xl mx-auto md:py-10 py-6 lg:mb-16 md:mb-10">
       <Swiper
         pagination={{ clickable: true }}
-        navigation={true} // 👈 add this line
-        modules={[Pagination, Navigation]} // 👈 include Navigation here
+        navigation={true}
+        modules={[Pagination, Navigation]}
         spaceBetween={30}
+        autoHeight={true} // 👈 add this line
         className="mySwiper"
       >
         <SwiperSlide>
@@ -88,41 +89,41 @@ function SuccessStoriesSlider() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="success-story rounded-4xl relative px-2 py-10 lg:px-20 md:px-12 shadow-lg">
-                      <h5 className="text-center md:text-2xl text-lg lg:text-3xl font-bold text-white mb-6">
-                        Trader's Success Story : From $225 to $3226
-                      </h5>
-          
-                      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center">
-                        <div className="order-2 md:order-1 space-y-4">
-                          <p className="text-gray-300 text-sm md:text-base">
-                            <span className="font-semibold text-[#00c4f4]">
-                              Arjun Mehta{" "}
-                            </span>
-                            from India turned a $50,000 FundedLake 2-Step account purchased
-                            for just $225 into a life-changing payout in just 8 days. What
-                            started as a simple challenge quickly transformed into a
-                            powerful reality: financial freedom, flexibility, and proof
-                            that with discipline and strategy, anything is possible.
-                          </p>
-                          <p className="text-gray-300 text-sm md:text-base">
-                            At FundedLake, stories like this are not rare—they are becoming
-                            the norm. Every payout is not just a withdrawal, it is a dream
-                            funded, a goal achieved, and a trader stepping into a whole new
-                            chapter of their life.
-                          </p>
-                        </div>
-          
-                        <div className="order-1 md:order-2 flex justify-center">
-                          <div className="w-full rounded-2xl overflow-hidden">
-                            <img
-                              src={certifiacteImg}
-                              alt="Certificate"
-                              className="rounded-2xl"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+            <h5 className="text-center md:text-2xl text-lg lg:text-3xl font-bold text-white mb-6">
+              Trader's Success Story : From $225 to $3226
+            </h5>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center">
+              <div className="order-2 md:order-1 space-y-4">
+                <p className="text-gray-300 text-sm md:text-base">
+                  <span className="font-semibold text-[#00c4f4]">
+                    Arjun Mehta{" "}
+                  </span>
+                  from India turned a $50,000 FundedLake 2-Step account
+                  purchased for just $225 into a life-changing payout in just 8
+                  days. What started as a simple challenge quickly transformed
+                  into a powerful reality: financial freedom, flexibility, and
+                  proof that with discipline and strategy, anything is possible.
+                </p>
+                <p className="text-gray-300 text-sm md:text-base">
+                  At FundedLake, stories like this are not rare—they are
+                  becoming the norm. Every payout is not just a withdrawal, it
+                  is a dream funded, a goal achieved, and a trader stepping into
+                  a whole new chapter of their life.
+                </p>
+              </div>
+
+              <div className="order-1 md:order-2 flex justify-center">
+                <div className="w-full rounded-2xl overflow-hidden">
+                  <img
+                    src={certifiacteImg}
+                    alt="Certificate"
+                    className="rounded-2xl"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
